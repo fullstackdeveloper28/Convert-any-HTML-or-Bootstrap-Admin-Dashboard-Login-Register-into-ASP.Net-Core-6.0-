0bb6213436a5a251ede04178a860bb867e1d050f
fullstackdeveloper28 <111575687+fullstackdeveloper28@users.noreply.github.com>
2024-01-14 22:08:35 +0530

    Full Stack Developer28
    Full Stack Developer28

---
+++ DashboardDemo/Controllers/DashboardController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DashboardDemo.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
